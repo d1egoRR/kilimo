94067aaf0c7054d979c5401247cc8544a93cd66a
d1egorr <driquelme@infoad.com.ar>
2020-09-13 21:50:49 -0300

    change dir locations

---
+++ rainfalls/urls.py
@@ -0,0 +1,44 @@
+"""rainfalls URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+
+from rest_framework import permissions
+from rest_framework.authtoken import views
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Kilimo Rainfall API's",
+      default_version='v1',
+      description="Kilimo Rainfall Apps API's",
+      terms_of_service="KIlimo",
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
+
+urlpatterns = [
+    path(
+        'swagger/',
+        schema_view.with_ui('swagger', cache_timeout=0),
+        name='schema-swagger-ui'
+    ),
+    path('admin/', admin.site.urls),
+    path('api-auth/', views.obtain_auth_token, name='api-auth'),
+]
