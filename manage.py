9a25635fff08aa8c400eb8868aa5b548af3425bc
d1egorr <driquelme@infoad.com.ar>
2020-09-16 21:51:28 -0300

    config settingas

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rainfalls.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rainfalls.settings.production')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
